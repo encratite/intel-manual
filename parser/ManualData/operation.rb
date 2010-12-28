59b2d1cc3be4e7bc3a84534ade8fb08d5ecb4bac
void <git.email.address@googlemail.com>
2010-12-28 09:44:59 +0100

    Fixed PCMPEQB/PCMPEQW/PCMPEQD

--- parser/ManualData/operation.rb
+++ parser/ManualData/operation.rb
@@ -209,7 +209,7 @@ class ManualData
       input += "\nFI;\nFI;"
     when 'MOVD/MOVQ', 'MOVS/MOVSB/MOVSW/MOVSD/MOVSQ', 'OUTS/OUTSB/OUTSW/OUTSD'
       replacements << convertToComments
-    when 'MOVQ', 'PADDQ', 'PADDSB/PADDSW', 'PADDUSB/PADDUSW', 'PAVGB/PAVGW'
+    when 'MOVQ', 'PADDQ', 'PADDSB/PADDSW', 'PADDUSB/PADDUSW', 'PAVGB/PAVGW', 'PCMPEQB/PCMPEQW/PCMPEQD'
       replacements += convertToCommentsCommon
     when 'NOP'
       return nil
